36dac627b03373a5b79b61a4e9b77c052af519eb
Matheus Rosa <matheusnsampaio@gmail.com>
2025-10-05 19:04:28 -0300

    feat: DB now works and inserts multiple rows at time

--- src/db/data_storage.py
+++ src/db/data_storage.py
@@ -14,37 +14,39 @@ class SragDb(MainLogger):
             
     @contextmanager
     def get_cursor(self):
-        if self.conn:
-            self.conn = sqlite3.connect("srag.db")
+        if self.conn is None:
+            self.conn = sqlite3.connect(self.db_file)
         
         cursor = None
         try:
             self.info("Creating cursor")
             cursor = self.conn.cursor()
             yield cursor
-            self.info("Commiting changes")
+            self.info("Committing changes")
             self.conn.commit()
         except Exception as e:
-            self.error("Error while using the cursor")
-            if cursor:
-                cursor.close()
-            raise e
-        finally:
-            self.info("Closing the connection")
+            self.error(f"Error during transaction: {e}. Rolling back changes.")
             if self.conn:
-                self.conn.close()
+                self.conn.rollback()
+            raise e  
+        finally:
+            if cursor:
+                self.info("Closing cursor")
+                cursor.close()  
 
     def _check_schema(self):
+        self.info("Checking the schema")
         with self.get_cursor() as cursor:
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS data_sus (
                     year INTEGER,
-                    SG_UF_NOT TEXT NOT NULL,
-                    DT_NOTIFIC DATETIME NOT NULL,
+                    SG_UF_NOT TEXT,
+                    DT_NOTIFIC DATETIME,
                     UTI INTEGER,
                     VACINA_COV INTEGER,
-                    HOSPITAL INTEGER
-                )
+                    HOSPITAL INTEGER,
+                    UNIQUE (year, SG_UF_NOT, DT_NOTIFIC, UTI, VACINA_COV, HOSPITAL)
+                );
             ''')
         return
     
@@ -55,14 +57,25 @@ class SragDb(MainLogger):
         
         insertion_query = """
             INSERT INTO data_sus (year, SG_UF_NOT, DT_NOTIFIC, UTI, VACINA_COV, HOSPITAL)
-            VALUES (:year, :SG_UF_NOT, :DT_NOTIFIC, :UTI, :VACINA_COV, :HOSPITAL)
+            VALUES (?, ?, ?, ?, ?, ?)
+            ON CONFLICT DO NOTHING
             """
         
         try:
             self.info("Starting insertion")
+            data_to_insert = [
+                (
+                    d['year'], 
+                    d['SG_UF_NOT'], 
+                    d['DT_NOTIFIC'], 
+                    d['UTI'], 
+                    d['VACINA_COV'], 
+                    d['HOSPITAL']
+                ) for d in data
+            ]
+
             with self.get_cursor() as cursor:
-                for row_data in data :
-                    cursor.execute(insertion_query, row_data)
+                cursor.executemany(insertion_query, data_to_insert)
             self.info("Insertion done")
             return True
         except Exception as e:
@@ -70,23 +83,22 @@ class SragDb(MainLogger):
             return False
         
     def get_data(self, year: str):
-        if year not in ["all", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]:
+        if year not in ["all", 2019, 2020, 2021, 2022, 2023, 2024, 2025]:
             self.error("Option not available")
             return None
         
         self.info("Creating the query")
-        query = "SELECT * FROM srag"
+        query = "SELECT * FROM data_sus"
 
         if year != "all":
             self.info("Adding the condition")
-            query += f"WHERE year = '{year}'"
+            query += f" WHERE year = {year}"
 
         try:
             self.info("getting the data")
-            with self.get_cursor() as cursor:
-                df = pd.read_sql_query(query, cursor)
+            df = pd.read_sql_query(query, self.conn)
             self.info(f"Retrieved the data: {df.shape[0]}")
             return df
         except Exception as e:
-            self.error("Error retrieving the data: {e}")
+            self.error(f"Error retrieving the data: {e}")
             return None
